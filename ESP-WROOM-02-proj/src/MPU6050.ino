0dc01ed1aa4928b21fb7951cdd1c452d8e097145
RyoYamada <newfield1001@gmail.com>
2017-08-10 00:04:45 +0900

    add src

---
+++ ESP-WROOM-02-proj/src/MPU6050.ino
@@ -0,0 +1,82 @@
+#include <Wire.h>
+#define DEVICE_ADDR 0x68  // Device Address
+#define ACCEL_XOUT  0x3b
+#define ACCEL_YOUT  0x3d
+#define ACCEL_ZOUT  0x3f
+#define GYRO_XOUT   0x43
+#define GYRO_YOUT   0x45
+#define GYRO_ZOUT   0x47
+#define PWR_MGMT_1  0x6b
+#define PWR_MGMT_2  0x6c
+#define WHO_AM_I    0x75
+
+#define MPU6050_SMPLRT_DIV   0x19 // MPU-6050 register address
+#define MPU6050_CONFIG       0x1a
+#define MPU6050_GYRO_CONFIG  0x1b
+#define MPU6050_ACCEL_CONFIG 0x1c
+
+double AccelX;
+double AccelY;
+double AccelZ;
+double GyroX;
+double GyroY;
+double GyroZ;
+
+void initializeMPU6050(){
+  Wire.begin();
+  writeMPU6050(PWR_MGMT_1, 0);
+}
+
+void writeMPU6050(byte addr, byte data) {
+  Wire.beginTransmission(DEVICE_ADDR);
+  Wire.write(addr);
+  Wire.write(data);
+  Wire.endTransmission(true);
+}
+
+byte readByteMPU6050(byte addr) {
+  Wire.beginTransmission(DEVICE_ADDR);
+  Wire.write(addr);
+  Wire.endTransmission(true);
+  Wire.requestFrom(DEVICE_ADDR, 1);
+  return Wire.read();
+}
+
+int16_t readWordMPU6050(byte addr) {
+  int8_t high;
+  int8_t low;
+  int16_t ret;
+  high = readByteMPU6050(addr);
+  low = readByteMPU6050(low);
+  ret = (high << 8) + low;
+  if (ret >= 0x8000) {
+    ret = -((65535 - ret) + 1);
+  }
+  return ret;
+}
+
+void readSensorData() {
+  AccelX = readWordMPU6050(ACCEL_XOUT) / 16384.0;
+  AccelY = readWordMPU6050(ACCEL_YOUT) / 16384.0;
+  AccelZ = readWordMPU6050(ACCEL_ZOUT) / 16384.0;
+  GyroX = readWordMPU6050(GYRO_XOUT) / 131.0;
+  GyroY = readWordMPU6050(GYRO_YOUT) / 131.0;
+  GyroZ = readWordMPU6050(GYRO_ZOUT) / 131.0;
+}
+
+void printSensorData() {
+  Serial.println("--------------");
+  Serial.print("Accel X:" + String(AccelX));
+  Serial.print(" Y:" + String(AccelY));
+  Serial.println(" Z:" + String(AccelZ));
+  Serial.print("Gyro X:" + String(GyroX));
+  Serial.print(" Y:" + String(GyroY));
+  Serial.println(" Z:" + String(GyroZ));
+}
+
+bool decideLED() {
+    if (AccelX > 0.5 ){
+        return true;
+    }
+    return false;
+}
---
+++ ESP-WROOM-02-proj/src/main.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#define LED_PIN 13
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  initializeMPU6050();
+  Serial.begin(115200);
+  while(!Serial){
+    // wait for serial port to connect
+    ;
+  }
+  Serial.println("Wake Up!!");
+}
+
+void loop() {
+  readSensorData();
+  printSensorData();
+  if (decideLED() > 0.5) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
+  delay(500);
+}
