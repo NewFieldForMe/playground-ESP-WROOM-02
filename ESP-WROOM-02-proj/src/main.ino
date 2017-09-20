2dbbc95e4e1fc0293b3d3220756e7dc75d46f06e
RyoYamada <newfield1001@gmail.com>
2017-09-21 00:29:53 +0900

    refactoring

--- ESP-WROOM-02-proj/src/main.ino
+++ ESP-WROOM-02-proj/src/main.ino
@@ -6,8 +6,6 @@
 #include "module/ESP8266-ST7735.h"
 #include "module/MPU6050.h"
 
-int8_t neko;
-
 ESP8266_Wifi* _wifi;
 char ssid[] = "";
 char password[] = "";
@@ -25,47 +23,25 @@ void setup() {
   // wifi start
   _wifi = new ESP8266_Wifi(ssid, password);
   _wifi->setup();
-  // pinMode(LED_PIN, OUTPUT);
-  // initializeMPU6050();
+  // ST7735 start
   _ST7735 = new ESP8266_ST7735();
   _ST7735->setup();
-
+  // MPU6050 start
   _MPU6050 = new MPU6050();
   _MPU6050->setup();
-
-  neko = 1;
 }
 
 void loop() {
   _wifi->loop();
   _MPU6050->loop();
-  // readSensorData();
-  // calcRotation();
-  // printRawSensorData();
-  // printCalcSensorData();
-  // printAngle();
-  // if (decideLED() > 0.5) {
-  //   digitalWrite(LED_PIN, HIGH);
-  // } else {
-  //   digitalWrite(LED_PIN, LOW);
-  // }
-  // sendSensorDataForMovingMouse();
-  // String str = stringAngle();
-  // char charArray[str.length()];
-  // str.toCharArray(charArray, str.length());
 
-  // drawtext(charArray, ST7735_WHITE);
-  
-  // ------
   String filename;
-  // if (neko == true) {
   if (_MPU6050->decideLED()){
     filename = "/mya.bmp";
   } else {
     filename = "/innu.bmp";
   }
   _ST7735->bmpDraw(filename, 0, 0);
-  neko = !neko;
 
   delay(1000);
 }
